dcb57ebd77616c1b357fcb285f723b2fa8e41348
Keyush Prajapati <102168904+KeyushPrajapati@users.noreply.github.com>
2023-10-07 21:59:23 +0530

    Add files via upload

---
+++ backend/djangreact/urls.py
@@ -0,0 +1,15 @@
+
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('djangobackend.urls')),  # Include your app's API URLs here
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
